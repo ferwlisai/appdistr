cd28eb341d02b6edd90c8010c557a137b3887ca6
ferwlisai <ferwlisai@gmail.com>
2026-04-14 20:02:26 -0300

    Update build.gradle.kts

--- ListaPreciosAndroid/app/build.gradle.kts
+++ ListaPreciosAndroid/app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("org.apache.poi:poi-ooxml:5.2.5")
     implementation("org.apache.poi:poi:5.2.5")
+    implementation("com.google.android.material:material:1.12.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
